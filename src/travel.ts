1dc8d0c8b1a6f5eb2c2e04011af1531b670e0d16
BraunRoland <rolibraun94@gmail.com>
2026-04-21 06:43:01 +0200

    sort

--- src/travel.ts
+++ src/travel.ts
@@ -2,32 +2,38 @@ import { Bucket } from "./Bucket.ts";
 import { Utazas } from "./Utazas.ts";
 let utazasArr: Utazas[] = [];
 let bucketArr: Bucket[] = [];
+let elozo: HTMLElement | null = null;
 
-function tablazatKiiras(): void {
+function tablazatKiiras(array: Utazas[]): void {
     const tablazat = document.getElementById('tableAdat') as HTMLTableSectionElement;
-    utazasArr.forEach((u: Utazas,id) => {
+    tablazat.innerHTML = '';
+    array.forEach((u: Utazas,id) => {
         const tr = document.createElement('tr') as HTMLTableRowElement;
         const hely = document.createElement('td') as HTMLTableCellElement;
         hely.textContent = u.city+ `, ${u.country}`;
+        hely.dataset.key = 'city';
         const kulonb = u.dateEnd.getTime() - u.dateStart.getTime();
         const ido = document.createElement('td') as HTMLTableCellElement;
+        ido.dataset.key = 'dateStart,dateEnd';
         ido.innerHTML = `${u.writeDateStart()} - <br>${u.writeDateEnd()}`
         const penz = document.createElement('td') as HTMLTableCellElement;
+        penz.dataset.key = 'price';
         penz.textContent = `${u.price.toLocaleString()} Ft`;
         const ertek = document.createElement('td') as HTMLTableCellElement;
         let ertekStrting: string = '';
         for (let i = 1; i < 6; i++) {
             if (i <= u.rating) {
-                ertekStrting += '&#9733; '
+                ertekStrting += '&#9733;'
             }
             else {
-                ertekStrting += '&#9734; '
+                ertekStrting += '&#9734;'
             }
             //&#9733; fekete
             //&#9734; ures
         }
         ertek.innerHTML = ertekStrting;
         ertek.className = 'csillag';
+        ertek.dataset.key = 'rating';
         tablazat.appendChild(tr);
         tr.appendChild(hely);
         tr.appendChild(ido);
@@ -36,8 +42,74 @@ function tablazatKiiras(): void {
     });
 }
 
-function reszletek(id: number): void {
-    console.log(utazasArr[id]);
+function ascClick(event: Event) {
+    const elem = event.currentTarget as HTMLElement;
+    const key = elem.dataset.key;
+    sortAsc(key!, elem);
+}
+
+function descClick(event: Event) {
+    const elem = event. currentTarget as HTMLElement;
+    const key = elem.dataset.key;
+    sortDesc(key!,elem);
+}
+
+function sortAsc(key: string, elem: HTMLElement): void {
+    let arr: Utazas[] = utazasArr;
+    if (key === 'city') {
+        arr.sort((a,b) => a[key].localeCompare(b[key]));
+    }
+    else if (key === 'rating' || key === 'price') {
+        arr.sort((a,b) => a[key] - b[key]);
+    }
+    else if (key === 'dateStart,dateEnd') {
+        const keys:string[] = key.split(',');
+        const targetKey = keys[0];
+        arr.sort((a,b) => {
+            const dateA = new Date((a as any)[targetKey!]).getTime();
+            const dateB = new Date((b as any)[targetKey!]).getTime();
+            return dateA - dateB;
+        })
+    }
+    elem.removeEventListener('click',ascClick);
+    elem.addEventListener('click',descClick);
+    console.log(elozo);
+    if (elozo != null) {
+        const clean = elozo.innerHTML.split(' ')
+        elozo.innerHTML = clean[0]!;
+    };
+    elem.innerHTML += ' &#8593;';
+    elozo = elem;
+    tablazatKiiras(arr);
+}
+
+function sortDesc(key: string, elem: HTMLElement) {
+    let arr: Utazas[] = utazasArr;
+    if (key === 'city') {
+        arr.sort((a,b) => b[key].localeCompare(a[key]));
+    }
+    else if (key === 'rating' || key === 'price') {
+        arr.sort((a,b) => b[key] - a[key]);
+    }
+    else if (key === 'dateStart,dateEnd') {
+        const keys:string[] = key.split(',');
+        const targetKey = keys[1];
+        arr.sort((a,b) => {
+            const dateA = new Date((a as any)[targetKey!]).getTime();
+            const dateB = new Date((b as any)[targetKey!]).getTime();
+            return dateB - dateA;
+        })
+    }
+    elem.removeEventListener('click',descClick);
+    elem.addEventListener('click',ascClick);
+    console.log(elozo);
+    if (elozo != null) {
+        const clean = elozo.innerHTML.split(' ')
+        elozo.innerHTML = clean[0]!;
+    }
+    elem.innerHTML += ' &#8595;';
+    elozo = elem;
+    tablazatKiiras(arr);
 }
 
 async function utazasLekerdezes() {
@@ -51,8 +123,6 @@ async function utazasLekerdezes() {
 
     return adatok;
 }
-
-
 
 async function bucketLekerdezes() {
         const adatok: Bucket[] = [];
@@ -70,13 +140,19 @@ async function frissites() {
     bucketArr = await bucketLekerdezes();
 }
 
-
+function eventAdder(): void {
+    const thArr = document.getElementsByTagName('th');
+    for(const elem of thArr) {
+        elem.addEventListener('click',ascClick)
+    }
+}
 
 async function init() {
     await frissites();
     console.log(utazasArr, bucketArr);
     console.log(bucketArr[0]?.writeDate(), utazasArr[0]?.writeDateStart(), utazasArr[0]?.writeDateEnd());
-    tablazatKiiras();
+    tablazatKiiras(utazasArr);
+    eventAdder();
 }
 
 document.addEventListener("DOMContentLoaded", init);
